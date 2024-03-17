18438bf68348e17a5b73f413e540ddfe54f9d199
leopaul29 <leopaulmartin.iut@gmail.com>
2024-03-17 14:56:31 +0100

    update updateFlow script

--- backend/scripts/updateFlow.ts
+++ backend/scripts/updateFlow.ts
@@ -19,6 +19,18 @@ async function main() {
 	// --- step 3 - display proposal tab
 	// comment if dont want more proposal
 	await voting.connect(owner).addProposal("first proposal");
+
+	// --- step 4 - end proposal registration
+	// comment if already at endProposalsRegistering wotkflow
+	await voting.connect(owner).endProposalsRegistering();
+
+	// --- step 5 - start voting session
+	// comment if already at startVotingSession wotkflow
+	await voting.connect(owner).startVotingSession();
+
+	// --- step 5 - voting session
+	// comment if already at voted
+	await voting.connect(owner).setVote(1);
 }
 
 main().catch((error) => {
